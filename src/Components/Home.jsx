76242ca0624ee4c8401d44aa1b11587295e8eb78
Amrita Prasad <amritaprasad1705@gmail.com>
2024-07-03 13:56:52 +0000

    bg 1

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,7 @@ import PropTypes from "prop-types";
  * freely use on your site.
  */
 
-import image from "../images/bg1.png";
+import image from "../images/bgg1.png";
 
 const imageAltText = "my photo with calming warm background";
 
