af7098196c43136de63519fdacdc99cf740ed8e3
Amrita Prasad <amritaprasad1705@gmail.com>
2024-07-03 13:59:52 +0000

    bgg2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/background2.jpeg";
+import image from "../images/bgg2.jpeg";
 
 const imageAltText = "back dark abstract background";
 
